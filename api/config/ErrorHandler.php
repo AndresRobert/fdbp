b4fc2cb4d523bc62155c50e204a6fb9cb28cc4df
AndresRobert <andresrobert@icloud.com>
2020-08-07 12:52:50 -0400

    init

--- api/config/ErrorHandler.php
+++ api/config/ErrorHandler.php
@@ -11,14 +11,15 @@ function json_error($severity, $message, $file, $line)
     http_response_code(500);
     header('Content-Type: application/json; charset=utf-8');
     $response = array_combine(['errno', 'errstr', 'errfile', 'errline', 'errcontext'], func_get_args());
-    die(json_encode($response, JSON_THROW_ON_ERROR));
+    die(json_encode(['status' => 'fail', 'response' => $response], JSON_THROW_ON_ERROR));
 }
 set_error_handler('json_error');
 
 function json_fatal()
 {
     $last_error = error_get_last();
-    if ( error_reporting() & $last_error['type'] )
-        call_user_func_array( 'json_error', $last_error );
+    if ( error_reporting() & $last_error['type'] ) {
+        json_error(...$last_error);
+    }
 }
 register_shutdown_function( 'json_fatal' );
