3cf94591389f3e3940d628ca36d7c8c123fe7ed7
AndresRobert <andresrobert@icloud.com>
2020-08-20 19:16:33 -0400

    preview

--- admin/preview/contract.php
+++ admin/preview/contract.php
@@ -5,61 +5,7 @@ $strDate = strftime("%e de %B del %G",$date->getTimestamp());
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <head><?php include '../layout/head.php' ?></head>
-    <script>
-
-        function setPrintable(_data) {
-            console.log(_data);
-
-            $('#s_name').html(setDefault(_data.s_name, 'Sin nombre'));
-            $('#s_lastname').html(setDefault(_data.s_lastname, 'Sin apellido'));
-            $('#s_address').html(setDefault(_data.s_address, 'Sin dirección'));
-            $('#s_mobile').html(setDefault(_data.s_mobile, '9 0000 0000'));
-            $('#s_email').html(setDefault(_data.s_email, 'sin_mail@mail.cl'));
-            $('#s_id').html(setDefault(_data.s_id, '00000000-K'));
-            $('#s_comune_name').html(setDefault(_data.s_comune_name, 'Sin Comuna'));
-            $('#s_region_name').html(setDefault(_data.s_region_name, 'Sin Region'));
-
-            $('#d_name').html(setDefault(_data.d_name, 'Sin nombre'));
-            $('#d_id').html(setDefault(_data.d_id, '00000000-K'));
-            $('#d_marital_status').html(setDefault(_data.d_marital_status, 'No informado'));
-            $('#d_address').html(setDefault(_data.d_address, 'Sin dirección'));
-            $('#d_place').html(setDefault(_data.d_place, 'No informado'));
-            $('#d_occupation').html(setDefault(_data.d_occupation, 'No informado'));
-            $('#d_education').html(setDefault(_data.d_education, 'No informado'));
-            $('#d_comune_name').html(setDefault(_data.d_comune_name, 'Sin Comuna'));
-            $('#d_region_name').html(setDefault(_data.d_region_name, 'Sin Region'));
-        }
-
-        $(() => {
-            let data = JSON.parse(_$.cookie.get('fdbp_contract_data'));
-            const urlParams = new URLSearchParams(location.search);
-            if (urlParams.get('contract') !== null) {
-                _$('#body').removeClass('preview');
-                _$.ajax('/api/contracts/get', { id: urlParams.get('contract') }, { headers: getBearerHeaders()}).then(
-                    ({ status, response }) => {
-                        if (status === 'error') {
-                            _$('#body').addClass('preview');
-                            _$.snackbar('Hubo un error al intentar guardar el contrato, cierre esta vista y vuelva a intentarlo', 'Cerrar');
-                        }
-                        if (status === 'fail') {
-                            _$.snackbar('Session expirada, cierre esta vista y vuelva a intentarlo', 'Cerrar');
-                        } else {
-                            if (response.status !== 'fail' && response.contract !== []) {
-                                data = response.contract;
-                            } else {
-                                _$.snackbar('Hubo un error al intentar guardar el contrato, cierre esta vista y vuelva a intentarlo', 'Cerrar');
-                            }
-                        }
-                    }
-                ).catch(e => console.log(e));
-            }
-            setPrintable(data);
-        });
-
-    </script>
-</head>
+<head><?php include '../layout/head.php' ?></head>
 <body id="body" class="preview">
 <div id="content" class="grid printable">
     <div class="row center">
@@ -193,7 +139,7 @@ $strDate = strftime("%e de %B del %G",$date->getTimestamp());
             <p>El saldo es pagado con:</p>
             <div class="row">
                 <div class="col-12 no-padding-input">
-                    <p><i id="p_transfer" class="far fa-check-square"></i> Transferencia</p>
+                    <p><i id="p_transfer" class="far fa-square"></i> Transferencia</p>
                 </div>
                 <div class="col-12 no-padding-input">
                     <p><i id="p_cash" class="far fa-square"></i> Efectivo</p>
@@ -223,5 +169,96 @@ $strDate = strftime("%e de %B del %G",$date->getTimestamp());
         </div>
     </div>
 </div>
+<script>
+
+    function setPrintable(_data) {
+        console.log(_data);
+
+        $('#s_name').html(setDefault(_data.s_name, 'Sin nombre'));
+        $('#s_lastname').html(setDefault(_data.s_lastname, 'Sin apellido'));
+        $('#s_address').html(setDefault(_data.s_address, 'Sin dirección'));
+        $('#s_mobile').html(setDefault(_data.s_mobile, '9 0000 0000'));
+        $('#s_email').html(setDefault(_data.s_email, 'sin_mail@mail.cl'));
+        $('#s_id').html(setDefault(_data.s_id, '00000000-K'));
+        $('#s_comune_name').html(setDefault(_data.s_comune_name, 'Sin Comuna'));
+        $('#s_region_name').html(setDefault(_data.s_region_name, 'Sin Region'));
+
+        $('#d_name').html(setDefault(_data.d_name, 'Sin nombre'));
+        $('#d_id').html(setDefault(_data.d_id, '00000000-K'));
+        $('#d_marital_status').html(setDefault(_data.d_marital_status, 'No informado'));
+        $('#d_address').html(setDefault(_data.d_address, 'Sin dirección'));
+        $('#d_place').html(setDefault(_data.d_place, 'No informado'));
+        $('#d_occupation').html(setDefault(_data.d_occupation, 'No informado'));
+        $('#d_education').html(setDefault(_data.d_education, 'No informado'));
+        $('#d_comune_name').html(setDefault(_data.d_comune_name, 'Sin Comuna'));
+        $('#d_region_name').html(setDefault(_data.d_region_name, 'Sin Region'));
+
+        $('#c_church').html(setDefault(_data.c_church, 'No informado'));
+        $('#c_address').html(setDefault(_data.c_address, 'Sin dirección'));
+        $('#c_comune_name').html(setDefault(_data.c_comune_name, 'Sin Comuna'));
+        $('#c_region_name').html(setDefault(_data.c_region_name, 'Sin Region'));
+
+        $('#v_service_name').html(setDefault(_data.v_service_name, 'No informado'));
+        $('#v_include').html(setDefault(_data.v_include, ''));
+        $('#v_warning').html(setDefault(_data.v_warning, ''));
+        $('#v_observation').html(setDefault(_data.v_observation, ''));
+
+        $('#f_cementery_name').html(setDefault(_data.f_cementery_name, 'No informado'));
+        $('#f_date').html(setDefault(_data.f_date, ''));
+        $('#f_time').html(setDefault(_data.f_time, ''));
+
+        $('#v_cost').html(setDefault(Number(_data.v_cost).toLocaleString('es-CL'), '0'));
+        $('#v_discount').html(setDefault(Number(_data.v_discount).toLocaleString('es-CL'), '0'));
+        $('#v_total').html(setDefault(Number(_data.v_total).toLocaleString('es-CL'), '0'));
+        $('#v_coverage').html(setDefault(Number(_data.v_coverage).toLocaleString('es-CL'), '0'));
+        $('#v_payment').html(setDefault(Number(_data.v_payment).toLocaleString('es-CL'), '0'));
+
+        if (_data.p_cash === true) {
+            $('#p_cash').removeClass('fa-square').addClass('fa-check-square');
+        }
+        if (_data.p_check === true) {
+            $('#p_check').removeClass('fa-square').addClass('fa-check-square');
+        }
+        if (_data.p_check_defered === true) {
+            $('#p_check_defered').removeClass('fa-square').addClass('fa-check-square');
+        }
+        if (_data.p_credit_card === true) {
+            $('#p_credit_card').removeClass('fa-square').addClass('fa-check-square');
+        }
+        if (_data.p_other === true) {
+            $('#p_other').removeClass('fa-square').addClass('fa-check-square');
+        }
+        if (_data.p_transfer === true) {
+            $('#p_transfer').removeClass('fa-square').addClass('fa-check-square');
+        }
+    }
+
+    $(() => {
+        let data = JSON.parse(_$.cookie.get('fdbp_contract_data'));
+        const urlParams = new URLSearchParams(location.search);
+        if (urlParams.get('contract') !== null) {
+            _$('#body').removeClass('preview');
+            _$.ajax('/api/contracts/get', { id: urlParams.get('contract') }, { headers: getBearerHeaders()}).then(
+                ({ status, response }) => {
+                    if (status === 'error') {
+                        _$('#body').addClass('preview');
+                        _$.snackbar('Hubo un error al intentar guardar el contrato, cierre esta vista y vuelva a intentarlo', 'Cerrar');
+                    }
+                    if (status === 'fail') {
+                        _$.snackbar('Session expirada, cierre esta vista y vuelva a intentarlo', 'Cerrar');
+                    } else {
+                        if (response.status !== 'fail' && response.contract !== []) {
+                            data = response.contract;
+                        } else {
+                            _$.snackbar('Hubo un error al intentar guardar el contrato, cierre esta vista y vuelva a intentarlo', 'Cerrar');
+                        }
+                    }
+                }
+            ).catch(e => console.log(e));
+        }
+        setPrintable(data);
+    });
+
+</script>
 </body>
 </html>
