ec8250ac341805bab8def4d0e1b8fa5521f4b9d9
AndresRobert <andresrobert@icloud.com>
2020-08-24 18:37:28 -0400

    refactor

--- app/layout/topbar.php
+++ app/layout/topbar.php
@@ -1,8 +1,12 @@
 <header>
     <img class="logo" src="../src/img/isotype.png" alt="" style="filter:grayscale(1) contrast(0.8)">
     <p style="font-family:'Great Vibes',cursive;font-size:26px">Funeraria del Buen Pastor</p>
+    <ul class="menu">
+        <li><a href="contracts.php" >Contratos</a></li>
+        <li><a href="quotations.php" >Cotizaciones</a></li>
+        <li><a href="admin.php" >Administración</a></li>
+    </ul>
     <ul class="icons">
         <li><i onclick="logout()" class="fas fa-power-off"></i></li>
-        <li><i onclick="toggleDarkMode()" class="fas fa-adjust"></i></li>
     </ul>
 </header>
