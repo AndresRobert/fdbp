249081e7e07082967aa51e6d99b763c83f4a4c8e
AndresRobert <andresrobert@icloud.com>
2020-08-24 18:39:31 -0400

    refactor

--- app/contracts.php
+++ app/contracts.php
@@ -1,1 +1,10 @@
-<?php header('Location: contracts.php');
+<!DOCTYPE html>
+<html lang="es">
+<head><?php include 'layout/head.php' ?></head>
+<body id="body">
+<?php include 'layout/topbar.php' ?>
+<div id="content" class="grid pv-1">
+    Content
+</div>
+</body>
+</html>
--- app/index.php
+++ app/index.php
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-<html lang="es">
-<head><?php include 'layout/head.php' ?></head>
-<body id="body">
-<?php include 'layout/topbar.php' ?>
-<div id="content" class="grid pv-1">
-    Content
-</div>
-</body>
-</html>
+<?php header('Location: contracts.php');
