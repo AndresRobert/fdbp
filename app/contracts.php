dfdefa5eed001494c6b44030101c04f19da4ac9d
AndresRobert <andresrobert@icloud.com>
2020-08-25 16:55:00 -0400

    list

--- app/contracts.php
+++ app/contracts.php
@@ -4,6 +4,7 @@
 <body class="grey lighten-3">
 <?php include 'layout/topbar.php' ?>
 <div class="container">
+    <a class="btn-floating btn-large waves-effect waves-light green tooltipped" data-position="left" data-tooltip="Nuevo Contrato"><i class="material-icons">add</i></a>
     <div class="row">
         <div class="col s12">
             <h4>Contratos</h4>
@@ -61,62 +62,9 @@
                     <th data-placeholder="none">Acciones</th>
                 </tr>
                 </thead>
-                <tbody id="contractsTable">
-                <tr>
-                    <td>1</td>
-                    <td>2020-08-12</td>
-                    <td>RUT 1</td>
-                    <td>Nombre 1</td>
-                    <td>Apellidos 1</td>
-                    <td>Dirección 1</td>
-                    <td>Comuna 1</td>
-                    <td>Región 1</td>
-                    <td>Celular 1</td>
-                    <td>Email 1</td>
-                    <td>Teléfono 1</td>
-                    <td>Nombres 1</td>
-                    <td>Dirección 1</td>
-                    <td>Comuna 1</td>
-                    <td>Región 1</td>
-                    <td>Estado Civil 1</td>
-                    <td>Estudios 1</td>
-                    <td>Ocupación 1</td>
-                    <td>Lugar de Fallecimiento 1</td>
-                    <td>Capilla 1</td>
-                    <td>Dirección 1</td>
-                    <td>Comuna 1</td>
-                    <td>Región 1</td>
-                    <td>Cementerio 1</td>
-                    <td>Fecha 1</td>
-                    <td>Hora 1</td>
-                    <td>Tipo 1</td>
-                    <td>Color 1</td>
-                    <td>Incluye 1</td>
-                    <td>Observación 1</td>
-                    <td>Aviso 1</td>
-                    <td>Valor 1</td>
-                    <td>Descuento 1</td>
-                    <td>Valor Total 1</td>
-                    <td>Previsión 1</td>
-                    <td>Previsión Aporte 1</td>
-                    <td>Saldo 1</td>
-                    <td data-search="si" data-sort="si"><i class="material-icons">check_box</i></td>
-                    <td data-search="si" data-sort="si"><i class="material-icons">crop_din</i></td>
-                    <td data-search="si" data-sort="si"><i class="material-icons">crop_din</i></td>
-                    <td data-search="si" data-sort="si"><i class="material-icons">check_box</i></td>
-                    <td data-search="si" data-sort="si"><i class="material-icons">crop_din</i></td>
-                    <td data-search="si" data-sort="si"><i class="material-icons">crop_din</i></td>
-                    <td>
-                        <button class="btn-flat waves-effect waves-googleBlue">Ver</button>
-                        <button class="btn-flat waves-effect waves-googleGreen">Publicar</button>
-                        <button class="btn-flat waves-effect waves-googleYellow">Editar</button>
-                        <button class="btn-flat waves-effect waves-googleRed">Eliminar</button>
-                    </td>
-                </tr>
-                </tbody>
+                <tbody id="contractsTable"></tbody>
             </table>
             <script>
-                // TODO: Load contracts
                 $.ajax({
                     url: Api.endpoints['contracts'],
                     type: 'get',
@@ -235,7 +183,6 @@
                         }
                     }
                 });
-                //$('#contractsTable').html();
             </script>
         </div>
     </div>
