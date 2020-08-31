f4871403826528b455deabc0224ee4729f251e5d
AndresRobert <andresrobert@icloud.com>
2020-08-31 19:47:24 -0400

    refactor

--- app/layout/head.php
+++ app/layout/head.php
@@ -1,6 +1,6 @@
 <?php
 $path = $_SERVER['SERVER_NAME'];
-$ver = '?v=1.2.0';
+$ver = '?v=1.3.0';
 ?>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://<?= $path ?>/src/base.css<?= $ver ?>" rel="stylesheet">
