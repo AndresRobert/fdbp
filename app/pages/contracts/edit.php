6fdc150a496f6248c899217b1eee7f20b518c791
AndresRobert <andresrobert@icloud.com>
2020-09-02 09:33:00 -0400

    Editar Contrato

--- app/pages/contracts/edit.php
+++ app/pages/contracts/edit.php
@@ -267,6 +267,7 @@
                         });
                     } else {
                         $('#editContract').modal('close');
+                        Helper.openLink();
                     }
                 }
             })
