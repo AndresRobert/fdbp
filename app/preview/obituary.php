8466eaa567bb99a2d2b099b243d8452db023f7bc
AndresRobert <andresrobert@icloud.com>
2020-09-08 11:48:19 -0300

    share

--- app/preview/obituary.php
+++ app/preview/obituary.php
@@ -1,6 +1,6 @@
 <?php
 setlocale(LC_ALL,"es_ES");
-$date = DateTime::createFromFormat("Y-m-d", date('Y-m-d'));
+$date = DateTime::createFromFormat("Y-m-d", $_GET['date']);
 $strDate = strftime("%e de %B del %G",$date->getTimestamp());
 ?>
 <!DOCTYPE html>
@@ -12,7 +12,24 @@ $strDate = strftime("%e de %B del %G",$date->getTimestamp());
 </head>
 
 <body>
-<pre><?php echo $_GET ?></pre>
+
+<div class="container">
+    <div class="card">
+        <div class="row">
+            <div class="col s12">T</div>
+        </div>
+        <div class="row">
+            <div class="col s12">Se comunica el sensible fallecimiento de <span><?php echo $_GET['name'] ?></span> (Q.E.P.D)</div>
+            <div class="col s12">El funeral se llevará a cabo en <span><?php echo $_GET['place'] ?></span> el dia <span><?php echo $strDate ?></span> a las <span><?php echo $_GET['time'] ?> horas.</span></div>
+            <div class="col s12"><span>"<?php echo $_GET['message'] ?>"</span></div>
+
+        </div>
+        <div class="row">
+            <div class="col s12">Obituario, gentileza <span>Funeraria del Buen Pastor</span></div>
+        </div>
+    </div>
+</div>
+
 </body>
 
 </html>
