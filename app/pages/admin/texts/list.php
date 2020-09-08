a68ea9447e479d6fd1c9546adfb119bc0741d61d
AndresRobert <andresrobert@icloud.com>
2020-09-08 18:05:27 -0300

    textos

--- app/pages/admin/texts/list.php
+++ app/pages/admin/texts/list.php
@@ -2,14 +2,12 @@
     Test
 </div>
 <script>
-    function getTexts() {
-        Api.post(Api.endpoints['texts'], true)
-            .then(({ status, response }) => {
-                if (status === 'OK') {
-                    M.toast({ html: response.message });
-                    console.log(response);
-                }
-            })
-            .catch( e => console.log(e) );
-    }
+    Api.post(Api.endpoints['texts'], true)
+        .then(({ status, response }) => {
+            if (status === 'OK') {
+                M.toast({ html: response.message });
+                console.log(response);
+            }
+        })
+        .catch( e => console.log(e) );
 </script>
