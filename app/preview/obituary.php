1ee268d820fd5255c76b74c0fb3a65faad3915ff
AndresRobert <andresrobert@icloud.com>
2020-09-08 19:09:12 -0300

    textos

--- app/preview/obituary.php
+++ app/preview/obituary.php
@@ -19,19 +19,48 @@ $strDate = strftime("%e de %B del %G",$date->getTimestamp());
             <div class="col s12"><img style="height:50px" src="/src/img/obi.png"></div>
         </div>
         <div class="row">
-            <div class="col s12">Se comunica el sensible fallecimiento de <span><?php echo $_GET['fdbpname'] ?></span> (Q.E.P.D)</div>
+            <div class="col s12"><span id="text_9">Se comunica el sensible fallecimiento de </span><?php echo $_GET['fdbpname'] ?> (Q.E.P.D)</div>
         </div>
         <div class="row">
-            <div class="col s12">El funeral se llevará a cabo en <span><?php echo $_GET['fdbpplace'] ?></span> el dia <span><?php echo $strDate ?></span> a las <span><?php echo substr($_GET['fdbptime'], 0, 5) ?> horas.</span></div>
+            <div class="col s12"><span id="text_10">El funeral se llevará a cabo en </span><?php echo $_GET['fdbpplace'] ?> el dia <span><?php echo $strDate ?></span> a las <span><?php echo substr($_GET['fdbptime'], 0, 5) ?> horas.</span></div>
         </div>
         <div class="row">
             <div class="col s12"><i>"<?php echo $_GET['fdbpmessage'] ?>"</i></div>
         </div>
         <div class="row">
-            <div class="col s12 gray-text" style="font-size:1rem">Obituario, gentileza <span>Funeraria del Buen Pastor</span></div>
+            <div class="col s12 gray-text" style="font-size:1rem"><span id="text_11">Obituario, gentileza de la Funeraria del Buen Pastor</span></div>
         </div>
     </div>
 </div>
+
+<script>
+    getTexts = async function () {
+        const response = await fetch('/api/texts/list', {
+            method: 'POST',
+            cache: 'no-cache',
+            referrerPolicy: 'no-referrer',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        return await response.json();
+    };
+
+    getTexts()
+        .then(({ status, response }) => {
+            if (status === 'OK') {
+                if (response.status === 'success') {
+                    $.each( response.list, function( _, text ) {
+                        if (text.id > 8 && text.id < 12) {
+                            $('#text_' + text.id).html(text.text);
+                        }
+                        if (text.id > 11) {
+                            return false;
+                        }
+                    });
+                }
+            }
+        })
+        .catch( e => console.log(e) );
+</script>
 
 </body>
 
