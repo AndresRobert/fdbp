24d198c0dacb0f9a66a5539dc2002238a9c1598f
AndresRobert <andresrobert@icloud.com>
2020-09-21 16:27:35 -0300

    eliminados

--- app/pages/contracts/list.php
+++ app/pages/contracts/list.php
@@ -192,6 +192,7 @@
                                                     btnEdit + btnDelete + btnInvoice + btnPayment + btnEmailSend + btnPublish;
                                             } else {
                                                 $(row).addClass("deleted");
+                                                console.log(row);
                                                 return 'Eliminado';
                                             }
                                         }
