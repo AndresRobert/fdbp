13c03fd7ba1b180b87b0dba76ad43b6dc886adf1
AndresRobert <andresrobert@icloud.com>
2020-09-29 19:32:47 -0300

    campos extra en contrato

--- app/layout/ext_head.php
+++ app/layout/ext_head.php
@@ -1,6 +1,6 @@
 <?php
 $path = $_SERVER['SERVER_NAME'];
-$ver = '?v=1.5.0';
+$ver = '?v=1.6.0';
 ?>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://<?= $path ?>/src/base.css<?= $ver ?>" rel="stylesheet">
